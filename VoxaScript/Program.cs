c0a1d46d45f5672a1041f4c24d5416e1868ecc94
Rebix <78218707+Reebix@users.noreply.github.com>
2023-08-21 15:35:03 +0200

    added simple tab complete

--- VoxaScript/Program.cs
+++ VoxaScript/Program.cs
@@ -1,26 +1,81 @@
-﻿using System.Security.Cryptography;
-using VoxaScript;
+﻿using Microsoft.VisualBasic.CompilerServices;
 using VoxaScript.Enviroment;
-using VoxaScript.Parser;
 
 var enviroment = new Enviroment();
 
-if (args.Length > 0 && args[0] == "--debug") { }
+if (args.Length > 0 && args[0] == "--debug")
+{
+}
+
+var comments = new[] {"exit", "new", "run"};
 
 // Green font color
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("VoxaScript 0.1.0");
 
-
 while (true)
-{
     try
     {
         // readline for command
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.Write(">> ");
-        string? consoleInput = Console.ReadLine();
-        string[]? command = consoleInput?.Split(" ");
+        Console.ForegroundColor = ConsoleColor.White;
+        var readKey = Console.ReadKey(true);
+        var consoleLine = "";
+        while (readKey.Key != ConsoleKey.Enter)
+        {
+            if (consoleLine.Length == 0 && readKey.Key == ConsoleKey.Backspace)
+            {
+                readKey = Console.ReadKey(true);
+                continue;
+            }
+
+            switch (readKey.Key)
+            {
+                case ConsoleKey.Backspace:
+                    consoleLine = consoleLine.Remove(consoleLine.Length - 1);
+                    Console.Write("\b \b");
+                    break;
+                case ConsoleKey.Tab:
+                    //TODO: improve this
+                    var c = consoleLine.Split(" ");
+                    if (c.Length == 1)
+                    {
+                        var suggestions = comments.Where(x => x.StartsWith(c[0])).ToList();
+                        if (suggestions.Count == 1)
+                        {
+                            var effectiveSuggestion = suggestions[0].Substring(c[0].Length);
+                            consoleLine += effectiveSuggestion;
+                            Console.Write(effectiveSuggestion);
+                        }
+                    }
+                    else
+                    {
+                        if(c[0] == "run")
+                        {
+                            var goUpDirs = "../../../Scripts/";
+                            var path = goUpDirs + c[1];
+                            var suggestions = Directory.GetFiles(goUpDirs).Where(x => x.StartsWith(path)).ToList();
+                            if (suggestions.Count == 1)
+                            {
+                                var effectiveSuggestion = suggestions[0].Substring(path.Length);
+                                consoleLine += effectiveSuggestion;
+                                Console.Write(effectiveSuggestion);
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    consoleLine += readKey.KeyChar;
+                    Console.Write(readKey.KeyChar);
+                    break;
+            }
+            
+            readKey = Console.ReadKey(true);
+        }
+
+        var command = consoleLine.Split(" ");
+        Console.WriteLine();
 
         switch (command?[0])
         {
@@ -28,15 +83,15 @@ while (true)
                 Console.ForegroundColor = ConsoleColor.White;
                 Environment.Exit(0);
                 break;
-            case "reload":
+            case "new":
                 enviroment = new Enviroment();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Reloaded enviroment.");
+                Console.WriteLine("Reloaded new enviroment.");
                 break;
             case "run":
-                string goUpDirs = "../../../Scripts/";
-                string path = goUpDirs + command[1];
-                string source = File.ReadAllText(path);
+                var goUpDirs = "../../../Scripts/";
+                var path = goUpDirs + command[1];
+                var source = File.ReadAllText(path);
 
                 Console.ForegroundColor = ConsoleColor.White;
                 enviroment.Load(source);
@@ -44,7 +99,7 @@ while (true)
             default:
                 // merge command into string
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                if (consoleInput != null) enviroment.Load(consoleInput);
+                if (readKey != null) enviroment.Load(consoleLine);
                 break;
         }
     }
@@ -52,4 +107,3 @@ while (true)
     {
         Console.WriteLine("Error: " + e.Message + "\n" + e.StackTrace);
     }
-}
